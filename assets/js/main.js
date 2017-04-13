07327cac11cad0d50723056c4df36ad4a4957904
barshamaharjan <barsha@parakhi.com>
2017-04-13 21:15:13 +0545

    changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -94,34 +94,55 @@
 				});
 
 			}
-
+			$('.modal-content > span').on('click',function(){
+			      $(this).parent().parent().attr("style","display:none");
+			})
 	});
 
 })(jQuery);
 
 
 // Get the modal
-var modal = document.getElementById('myModal');
 
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+// var modal = document.getElementById('myModal');
+
+// Get the buttons that opens the modal
+// var btn = document.getElementById("myBtn");
+var btns = document.getElementsByClassName("model-show");
+    for(var i=0; i<btns.length; i++){
+				// When the user clicks on the button, open the modal
+				btns[i].addEventListener("click", function(){
+						// modal.style.display = "block";
+						var modelId = this.getAttribute("data-model");
+						var modal = document.getElementById(modelId);
+						if(modal != null){
+								modal.style.display = "block";
+							}
+						// console.log(,"btns[i]");
+				  });
+				// onclick = function() {
+				//     modal.style.display = "block";
+				// }
+    }
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+// var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks on the button, open the modal 
-btn.onclick = function() {
-    modal.style.display = "block";
-}
+// // When the user clicks on the button, open the modal
+// btn.onclick = function() {
+//     modal.style.display = "block";
+// }
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
-}
+// span.onclick = function() {
+//     modal.style.display = "none";
+// }
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
+// window.onclick = function(event) {
+// 	console.log(event.target);
+// 	console.log(event.target);
+//     // if (event.target == modal) {
+//     //     modal.style.display = "none";
+//     // }
+// }
